43878d833963e968b7e758ba024d44c94f8dd584
Akash Manna <makash1167@gmail.com>
2018-06-11 10:37:53 +0530

    Converting Celsius to Farhenheit and Swapping two numbers

---
+++ far_cel.py
@@ -0,0 +1,6 @@
+#Convert a temperature in Celsius to its equivalent Farhenheit
+celsius = int(input("Enter the temperature in Celsius: "))
+print ("The Temperature in Celsius is {celsius}". format(celsius=celsius))
+farhenheit = ((9 * celsius + 160) /5)
+#print ("The Temperature in farhenheit is {farhenheit}". format(farhenheit=farhenheit))
+print ("The Temperature in Farhenheit is",farhenheit)
---
+++ swap.py
@@ -0,0 +1,9 @@
+first = int(input("Enter the First Number: "))
+second = int(input("Enter the Second Number"))
+print("Before Swap")
+print ("The First Number is ",first," and the Second Number is ",second)
+first = first + second
+second = first - second
+first = first - second
+print ("After Swap")
+print ("The First Number is ",first," and the Second Number is ",second)
